781a2e5feb1806bd9fa47fff40ae1cb2adab7949
Rubens Batista <batistarubinho@gmail.com>
2021-08-26 00:29:47 -0300

    feat: add buttons on home's header

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,9 +1,16 @@
+import 'styles/pages/home.css';
+
 import { AppHeader } from 'components/app-header';
+
+import { HeaderButtons } from './header-buttons';
 
 function Home() {
   const fragment = document.createDocumentFragment();
 
-  fragment.appendChild(AppHeader());
+  const $headerButtons = HeaderButtons();
+  const $header = AppHeader({ children: $headerButtons });
+
+  fragment.appendChild($header);
 
   return fragment;
 }
