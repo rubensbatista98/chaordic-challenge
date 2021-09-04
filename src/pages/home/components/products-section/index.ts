77167078bcb278bb4727db100b4a18dea52bf948
Rubens Batista <batistarubinho@gmail.com>
2021-09-04 00:40:12 -0300

    feat: render products from API

--- src/pages/home/components/products-section/index.ts
+++ src/pages/home/components/products-section/index.ts
@@ -1,38 +1,37 @@
 import { Button } from 'components/button';
 import { ProductCard } from 'components/product-card';
 import { createElement } from 'utils/create-element';
+import { getProducts } from 'services/api/products';
 
 import './styles.css';
 
-const PRODUCTS_DATA = Array.from({ length: 5 }).map(() => ({
-  image: 'https://source.unsplash.com/random/130x130',
-  name: 'Nome do produto',
-  description: `
-      Descrição do produto um pouco maior, com duas linhas ou
-      três que explica melhor do que se trata.
-    `,
-  oldPrice: 'R$23,99',
-  price: 'R$19,99',
-  installments: {
-    count: 2,
-    value: 'R$9,99'
-  }
-}));
-
 function ProductsSection() {
-  const $productsList = createElement('div', { class: 'products-list' });
+  const $productsList = createElement('div', {
+    class: 'products-list',
+    'data-products': '',
+    'aria-atomic': 'true',
+    'aria-live': 'true'
+  });
+
   const $buttonMore = Button({
     class: 'button-more',
     variant: 'secondary',
+    'data-loading': '',
     children: 'Ainda mais produtos aqui!'
   });
+
   const $section = createElement('section', {
     id: 'products-section',
     class: 'home-section',
+    'aria-busy': 'true',
     children: '<h2 class="title">Sua seleção especial</h2>'
   });
 
-  $productsList.append(...PRODUCTS_DATA.map(ProductCard));
+  getProducts().then(({ products }) => {
+    $productsList.append(...products.map(ProductCard));
+    $buttonMore.removeAttribute('data-loading');
+  });
+
   $section.append($productsList, $buttonMore);
 
   return $section;
