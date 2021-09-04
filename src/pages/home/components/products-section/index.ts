9c35e8eda23d2ab6414bfb01712427cba363c4e7
Rubens Batista <batistarubinho@gmail.com>
2021-09-04 00:52:27 -0300

    feat: add products pagination

--- src/pages/home/components/products-section/index.ts
+++ src/pages/home/components/products-section/index.ts
@@ -3,7 +3,52 @@ import { ProductCard } from 'components/product-card';
 import { createElement } from 'utils/create-element';
 import { getProducts } from 'services/api/products';
 
+import type { Product } from 'types/product';
+
 import './styles.css';
+
+function insertProductsIntoDom(products: Product[]) {
+  const $productsList = document.querySelector('[data-products]');
+
+  if ($productsList === null) return;
+
+  const $cards = products.map(ProductCard);
+  const isPaginatedProducts = $productsList.childElementCount > 0;
+
+  $productsList.append(...$cards);
+
+  if (isPaginatedProducts) {
+    $cards[0].scrollIntoView();
+  }
+}
+
+function handleNextPageOrRemoveButton(
+  $button: HTMLButtonElement,
+  nextPage?: typeof getProducts | null
+) {
+  if (nextPage) {
+    $button.removeAttribute('data-loading');
+    $button.addEventListener('click', handleSeeMore(nextPage), {
+      once: true
+    });
+
+    return;
+  }
+
+  $button.remove();
+}
+
+function handleSeeMore(getMoreProducts: typeof getProducts) {
+  return async (event: MouseEvent) => {
+    const $button = event.target as HTMLButtonElement;
+    $button.dataset.loading = '';
+
+    const { products, nextPage } = await getMoreProducts();
+
+    insertProductsIntoDom(products);
+    handleNextPageOrRemoveButton($button, nextPage);
+  };
+}
 
 function ProductsSection() {
   const $productsList = createElement('div', {
@@ -27,9 +72,9 @@ function ProductsSection() {
     children: '<h2 class="title">Sua seleção especial</h2>'
   });
 
-  getProducts().then(({ products }) => {
-    $productsList.append(...products.map(ProductCard));
-    $buttonMore.removeAttribute('data-loading');
+  getProducts().then(({ products, nextPage }) => {
+    insertProductsIntoDom(products);
+    handleNextPageOrRemoveButton($buttonMore, nextPage);
   });
 
   $section.append($productsList, $buttonMore);
